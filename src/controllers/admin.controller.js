8729f944f6ee3eb56be216b227a827e4cbe91208
Diintech7 <diintechteam7@gmail.com>
2026-08-01 18:13:13 +0530

    client dashboard update

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -95,7 +95,7 @@ export const getAdmins = async (req, res, next) => {
 export const getAdminClients = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const clients = await User.find({ role: ROLES.CLIENT, assignedAdmin: id }).select("name email createdAt approvalStatus");
+    const clients = await User.find({ role: ROLES.CLIENT, assignedAdmin: id }).select("name email role createdAt approvalStatus");
     successResponse(res, clients, "Admin clients fetched successfully");
   } catch (err) {
     next(err);
@@ -105,7 +105,7 @@ export const getAdminClients = async (req, res, next) => {
 // Get clients assigned to the currently logged in Admin (Admin only)
 export const getMyClients = async (req, res, next) => {
   try {
-    const clients = await User.find({ role: ROLES.CLIENT, assignedAdmin: req.user._id }).select("name email createdAt approvalStatus");
+    const clients = await User.find({ role: ROLES.CLIENT, assignedAdmin: req.user._id }).select("name email role createdAt approvalStatus");
     successResponse(res, clients, "Your clients fetched successfully");
   } catch (err) {
     next(err);
